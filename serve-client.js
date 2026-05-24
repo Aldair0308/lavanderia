87b9ccb5b2a44bb5dacf10d89f127e2e0f518123
Aldair0308 <aldairm0308@gmail.com>
2026-05-23 21:14:06 -0600

    Fix client distDir to /app/dist

--- serve-client.js
+++ serve-client.js
@@ -2,7 +2,7 @@ const http = require('http');
 const fs = require('fs');
 const path = require('path');
 
-const distDir = path.join(__dirname, 'apps', 'client', 'dist');
+const distDir = path.join(__dirname, 'dist');
 const port = parseInt(process.env.PORT || '3000', 10);
 
 const MIME_TYPES = {
